08be32fd204f2c7147ee1abc08b6618f1526f9b0
“Thomas <thomas_vracer2@hotmail.com>
2020-06-17 10:30:38 +0200

    Aanbieden artikel: ArtikelService refactor.

--- src/app/services/artikel.service.ts
+++ src/app/services/artikel.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {Artikel} from '../models/artikel';
 import {GebruikerService} from './gebruiker.service';
 import {Router} from '@angular/router';
+import {BijlageService} from './bijlage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,45 +12,38 @@ export class ArtikelService {
 
   constructor(private httpClient: HttpClient,
               private gebruikerService: GebruikerService,
+              private bijlageService: BijlageService,
               private router: Router) {
   }
 
   url = 'http://localhost:9080/artikelen/nieuw';
 
-  // TODO: deze bende refactoren
   aanbiedenArtikel(artikel: Artikel, bijlage: any) {
-    console.log('Artikel received!');
-    artikel.gebruikerId = this.gebruikerService.huidigeGebruiker.id;
-    artikel.prijs = artikel.prijs.replace(',', '.');
-    delete artikel.bijlage;
-    console.log(artikel);
-    const artikelPromise = this.httpClient.post<Artikel>(this.url, artikel).toPromise();
+    artikel = this.verwerkArtikel(artikel);
+    const artikelPromise = this.verstuurArtikel(artikel);
+
     if (bijlage) {
       artikelPromise.then(response => {
-        const artikelId = response.id;
-        this.verstuurBijlage(bijlage, artikelId);
-      }, error => console.log(error));
+          const artikelId = response.id;
+          this.bijlageService.verstuurBijlage(bijlage, artikelId);
+        },
+        error => console.log(error));
     } else {
       artikelPromise.then(() => {
-        this.router.navigate(['artikelen/nieuw/succes']);
-      }, error => console.log(error));
+          this.router.navigate(['artikelen/nieuw/succes']);
+        },
+        error => console.log(error));
     }
   }
 
-  private verstuurBijlage(bijlage: any, artikelId: any) {
-    const url = this.url + '/bijlage';
-    const data = bijlage.item(0);
-    const type = bijlage.item(0).type;
-    const naam = bijlage.item(0).name;
-    console.log('Bijlage ontvangen' + naam + type + data);
-    let params = new HttpParams();
-    params = params.append('bijlagenaam', naam);
-    params = params.append('bijlagetype', type);
-    params = params.append('artikelid', artikelId);
-    this.httpClient.post(url, data, {params, responseType: 'text'})
-      .subscribe(
-        resp => this.router.navigate(['artikelen/nieuw/succes']),
-        error => this.router.navigate(['artikelen/nieuw/error'])
-      );
+  private verstuurArtikel(artikel: Artikel) {
+    return this.httpClient.post<Artikel>(this.url, artikel).toPromise();
+  }
+
+  private verwerkArtikel(artikel: Artikel) {
+    artikel.gebruikerId = this.gebruikerService.huidigeGebruiker.id;
+    artikel.prijs = artikel.prijs.replace(',', '.');
+    delete artikel.bijlage;
+    return artikel;
   }
 }
--- src/app/services/bijlage.service.ts
+++ src/app/services/bijlage.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +8,10 @@ import {Injectable} from '@angular/core';
 export class BijlageService {
 
   private BIJLAGE_MAX_SIZE = 10000000;
+  url = 'http://localhost:9080/artikelen/nieuw/bijlage';
 
-  constructor() {
+  constructor(private httpClient: HttpClient,
+              private router: Router) {
   }
 
   public checkBestandsGrootte(bestand: any) {
@@ -21,5 +25,24 @@ export class BijlageService {
   }
 
 
+  public verstuurBijlage(bijlage: any, artikelId: any) {
+    const data = bijlage.item(0);
+    const type = bijlage.item(0).type;
+    const naam = bijlage.item(0).name;
 
+    const params = this.setParams(naam, type, artikelId);
+    this.httpClient.post(this.url, data, {params, responseType: 'text'})
+      .subscribe(
+        resp => this.router.navigate(['artikelen/nieuw/succes']),
+        error => this.router.navigate(['artikelen/nieuw/error'])
+      );
+  }
+
+  private setParams(naam, type, artikelId: any) {
+    let params = new HttpParams();
+    params = params.append('bijlagenaam', naam);
+    params = params.append('bijlagetype', type);
+    params = params.append('artikelid', artikelId);
+    return params;
+  }
 }
